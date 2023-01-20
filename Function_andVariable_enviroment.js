45e21ecf01ccacf220b58c78bae7599bad612c08
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-20 14:25:13 +0530

    function and variable enviroment

---
+++ Function_andVariable_enviroment.js
@@ -0,0 +1,21 @@
+
+
+
+var a = 1;
+x(); //these functions are working because of hoisting
+y()
+function x(){
+    var a = 10;
+    console.log(a)//it will print 10 because when x function is called and brand new function execution context is created and in the first phase
+    //(memory creation phase) the brand x is created when and got undefined in the first phase and in the second phase 10 is assigned to a
+    //when it willl see console.log(a) it will print 10 after that it will return back to the line where it called 
+}
+
+
+function y(){
+    var a = 100;
+    console.log(a);
+} 
+
+
+console.log(a); // here it will print 1 because first it see in its memory creation phase it will find there is a = 1 so it will print 1
