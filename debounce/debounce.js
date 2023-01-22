7c4d59f9b5f925083bdf57c4f068a5c701152a45
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-22 15:03:40 +0530

    debounce example

---
+++ debounce/debounce.js
@@ -0,0 +1,18 @@
+
+let counter =0;
+function getData(e){
+let val = document.getElementById('search').value
+console.log('data fetching',val)
+}
+
+let myBetterFunction = debounce(getData,1000);
+myBetterFunction()
+function debounce(fn,delay){
+ let timerid;
+ return function(){
+    clearTimeout(timerid);
+    timerid = setTimeout(()=>{
+           fn();
+    },delay)
+ }
+}
