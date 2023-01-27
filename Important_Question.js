99254edc3a3f3e9873a6a9d2466ee5367cce9ff4
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-27 14:32:30 +0530

    palindrome number

--- Important_Question.js
+++ Important_Question.js
@@ -2,8 +2,8 @@
 
 function calculatesum(){
 let sum = 0;
-    while(num>0 || sum>9){
-        if(num==0){
+    while(num>0 || sum>9){ // we are doing this because we want a number of only one digit if the sum>9 || num>0 then we do num==sum and sum = 0
+        if(num==0 && sum>9){
             num = sum;
             sum = 0;
         }
@@ -13,10 +13,26 @@ let sum = 0;
     }
     return sum;
 }
-let num = 543154;
-
+   let num = 54;
 
  console.log(calculatesum(num))
 
 
+ // Program to check the whether the number is palindrome or not
 
+
+  function checkPalindrome(num){
+    if(num<0){
+        return false;
+    }
+    let temp = num;
+    let reverse = 0;
+    while(num>0){
+        let rem = num%10;
+        reverse = (reverse*10)+rem;
+        num = Math.floor(num/10);
+    }    
+    return temp===reverse
+ }
+
+console.log(checkPalindrome(101))
---
+++ shallow_CopyAndDeep_copy.jsx
@@ -0,0 +1,28 @@
+/*
+ Shallow copy :- shallow copy means tht only first level of the object are copied and deeper levels are referenced.
+ As you can see in this code snippet:
+After updating a property in the first level of the cloned objects, the original property is not updated.
+After updating a property in a deeper level, the original property is also updated. This happens because, in this case, deeper levels are referenced, not copied.
+*/
+
+let obj = {
+    name : "akshansh",
+    age : {
+        is : 25
+    }
+}
+
+let obj2 = {...obj};
+console.log(obj2)
+
+obj2.name = "adarsh";
+obj2.age.is = 48;
+
+console.log(obj)
+console.log(obj2)
+
+/*
+    Deep Copy :- Deep copy means that all level of the object are copied . This is a true copy of the object.
+
+
+*/
