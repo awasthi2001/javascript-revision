8f48b10d43555832360381dd2e1120b72cb34123
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-27 12:57:31 +0530

    sum of number until it becomes single number

--- Important_Question.js
+++ Important_Question.js
@@ -1,14 +1,22 @@
-//Program to find the sum of digits of a number until the sum becomes a single digit
+// program to find the sum of the Number until it becomes single number
 
-let num = 5431; //9
-
+function calculatesum(){
 let sum = 0;
-
-while(num>0){
-    let rem  = num % 10;
-    sum = (sum*10)+rem;
-    // sum += () rem;
-    num = num/10;
+    while(num>0 || sum>9){
+        if(num==0){
+            num = sum;
+            sum = 0;
+        }
+        let rem  = num%10;
+        sum += rem;
+        num = Math.floor(num/10);
+    }
+    return sum;
 }
+let num = 543154;
 
-console.log(sum)
+
+ console.log(calculatesum(num))
+
+
+
