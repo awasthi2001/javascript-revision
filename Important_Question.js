af7c83f4f57d421be43c2e6ed77c31e994c46f0c
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-29 20:33:41 +0530

    reverse the array in place

--- Important_Question.js
+++ Important_Question.js
@@ -1,19 +1,19 @@
 // program to find the sum of the Number until it becomes single number
 
-function calculatesum(){
-let sum = 0;
-    while(num>0 || sum>9){ // we are doing this because we want a number of only one digit if the sum>9 || num>0 then we do num==sum and sum = 0
-        if(num==0 && sum>9){
-            num = sum;
-            sum = 0;
-        }
-        let rem  = num%10;
-        sum += rem;
-        num = Math.floor(num/10);
-    }
-    return sum;
+function calculatesum(num){
+ let sum = 0;
+ while(num>0 || sum>9){
+  if(num==0 && sum>9){// we are doing this because we want a number of only one digit if the sum>9 || num>0 then we do num==sum and sum = 0
+    num = sum;
+    sum = 0;
+  }
+  let rem = num%10;
+  sum+=rem;
+  num = Math.floor(num/10);
+ }
+ return sum;
 }
-   let num = 54;
+   let num = 5454;
 
  console.log(calculatesum(num))
 
@@ -25,14 +25,35 @@ let sum = 0;
     if(num<0){
         return false;
     }
-    let temp = num;
+    let temp  = num;
     let reverse = 0;
-    while(num>0){
+    while(num!=0){
         let rem = num%10;
         reverse = (reverse*10)+rem;
         num = Math.floor(num/10);
-    }    
-    return temp===reverse
+    }
+    return temp == reverse;
  }
 
-console.log(checkPalindrome(101))
+console.log(checkPalindrome(10101))
+
+
+// Reverse the Array inPlace 
+
+let arr = [1,2,3,4,5,6,7,8,1]
+
+function swap(arr,a,b){
+    let temp  = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+let i = 0,j=arr.length-1;
+while(i<j){
+    swap(arr,i,j);
+    i++;
+    j--;
+}
+
+console.log(arr);
+
+
