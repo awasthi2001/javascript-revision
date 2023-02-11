a9b19bc60f249b3579fdda5dd2397baa07e21705
awasthi2001 <akshanshawasthi013@gmail.com>
2023-02-11 13:29:48 +0530

    default values

--- Es6_Examples.js
+++ Es6_Examples.js
@@ -12,7 +12,7 @@ console.log(sum(80,50))//and if i am not passing the marks of science .. It will
 
 //To do this we need to use default values
 
-function sum2(maths=0,eng=0,science=0){
+function sum2(maths=0,eng=0,science=0){ 
     return maths+eng+science;
 }
 console.log(sum2(80,50)) //The output will be 130 , because it will take the number of science is 0
