b90c97131dff8037bb70935eddcb57dddf849f95
awasthi2001 <akshanshawasthi013@gmail.com>
2023-03-24 15:26:39 +0530

    debouncing revision

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -1,18 +1,48 @@
+// console.log('it shows results as you type')
+//The debounce method do not run when invoked instead it wait for a predetermined time until executing . When we call the same process agsain
+//the previous process is cancelled and timer is reset
 
-let counter =0;
-function getData(e){
-let val = document.getElementById('search').value
-console.log('data fetching',val)
+//let btn = document.querySelector("button")
+let inp = document.querySelector("input")
+let mybetterfn = debounce(makeApiCall,700)
+inp.addEventListener("keyup",mybetterfn)
+let c = 0;
+function debounce(fn,delay){
+  let timerid;
+  return function(){
+    console.log(c++)
+   clearTimeout(timerid)
+   timerid = setTimeout(()=>{
+       fn()
+   },delay)
+  }
+}
+async function makeApiCall(){
+let container = document.getElementById("container")
+container.style.display = 'none'
+   let name = document.querySelector("input").value;
+   // console.log(name)
+   //    console.log(c++)
+   let res = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=4e3b25ef&s=${name}`)
+   let data = await res.json()
+   display(data.Search)
+//   console.log(data)
 }
 
-let myBetterFunction = debounce(getData,1000);
-
-function debounce(fn,delay){
- let timerid;
- return function(){
-    clearTimeout(timerid);
-    timerid = setTimeout(()=>{
-           fn();
-    },delay)
+let container = document.getElementById("container")
+container.style.display = 'none'
+  function display(data){
+  let container = document.getElementById("container")
+  container.innerHTML = ''
+ if(data){
+   container.style.display = 'block'
  }
+ data && data.forEach(function(ele){
+   let child = document.createElement("div")
+   child.innerHTML = `
+    <img src=${ele.Poster}  />
+   `
+   container.appendChild(child)
+  })
+
 }
