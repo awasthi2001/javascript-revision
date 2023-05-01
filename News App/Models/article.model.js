2240425a1ebabc30e7b4e3281a4acb7ff4c39129
awasthi2001 <akshanshawasthi013@gmail.com>
2023-05-01 14:06:51 +0530

    articleSchema added

---
+++ News App/Models/article.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const articleSchema = mongoose.Schema(
+    {
+      user_id : {type:String,required:true},
+      category : {type:String,required:true},
+      Article : {type:String,required:true}
+    },
+    {
+       versionKey : false
+    }
+)
+
+
+export let ArticleModel  = mongoose.model('article',articleSchema)
