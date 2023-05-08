efac1be6697d4636ea4c8ca35093d07e51ad8d0e
awasthi2001 <akshanshawasthi013@gmail.com>
2023-05-08 13:51:14 +0530

    Abstraction

--- OOPS/index.js
+++ OOPS/index.js
@@ -64,3 +64,33 @@ const user1 = new User("ak",10)
 console.log(user1.userName)
 user1.userScore = 20
 console.log(user1.userScore)
+
+
+//Abstraction (Data Protection)
+ 
+class User2{
+    constructor(){
+        if(this.constructor===User2){
+            throw new Error("Object of Abstract class cannot be created")
+        }
+    }
+    run(){
+        console.log("He is running")
+    }
+    walk(){
+        console.log("He is walking")
+    }
+}
+
+//let user5 = new User2();
+
+// we will use inheritance to use the properties of the  abstract class
+
+class Manager extends User2{
+    constructor(){
+        super()
+    }
+}
+
+let manager = new Manager();
+manager.run()
